e28ca71e7cc7ea1f29c0693f3a5588bcdd749541
Marc Caramello <marcc@clemson.edu>
2022-09-23 20:53:22 -0400

    Currently working on checkEnd()

--- index.js
+++ index.js
@@ -31,7 +31,30 @@ const aiGo = () => {
 // return X, O, or - if game is over
 // returns false if game isnt over
 const checkEnd = () => {
-
+	if (board[0] == board[1] == board[2]) {
+		return board[0];
+	}
+	else if (board[3] == board[4] == board[5]) {
+		return board[3];
+	}
+	else if (board[6] == board[7] == board[8]) {
+		return board[6];
+	}
+	else if (board[0] == board[3] == board[6]) {
+		return board[0];
+	}
+	else if (board[1] == board[4] == board[7]) {
+		return board[1];
+	}
+	else if (board[2] == board[5] == board[8]) {
+		return board[2];
+	}
+	else if (board[0] == board[4] == board[8]) {
+		return board[0];
+	}
+	else if (board[2] == board[4] == board[6]) {
+		return board[2];
+	}
 }
 
 // ✓ always sets aiFirst button to disabled
